b5ff0bf49bd3a56eba1e63d4fd8c402eff31c725
ryulational <97367422+ryulational@users.noreply.github.com>
2021-12-25 21:22:55 -0800

    Add feed endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,55 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
+use feed_rs::parser;
+use serde::Deserialize;
+use serde_json::json;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(Deserialize)]
+struct PostBody {
+    url: String,
+}
+
+async fn feed_info(body: web::Json<PostBody>) -> Result<String> {
+    let url: &String = &body.url;
+    let content = reqwest::get(url)
+        .await
+        .expect("Could not get request")
+        .bytes()
+        .await
+        .expect("Could not get bytes");
+
+    let feed = parser::parse(&content[..]).expect("Could not parse");
+
+    let title_content = feed.title.expect("Could not get title");
+    let title = title_content.content;
+
+    let description_content = feed.description.expect("Could not get updated");
+    let description = description_content.content;
+
+    let updated_content = feed.updated.expect("Could not get updated");
+    let updated = updated_content;
+
+    let feed = json!({
+        "title": title,
+        "description": description,
+        "updated": updated.to_rfc3339()
+    });
+
+    Ok(feed.to_string())
+}
+
 pub fn run() -> std::io::Result<Server> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/feed", web::post().to(feed_info))
+    })
+    .bind("127.0.0.1:8000")?
+    .run();
 
     Ok(server)
 }
