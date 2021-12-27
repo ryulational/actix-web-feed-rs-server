7d91a3fbc337f2501218ce4c386063f6afbd7b5e
ryulational <97367422+ryulational@users.noreply.github.com>
2021-12-26 22:16:02 -0800

    Update tests to use TestApp struct

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,13 @@
+use rss_server::startup::run;
 use std::net::TcpListener;
 
 #[actix_rt::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get(&format!("{}/health_check", &address))
+        .get(&format!("{}/health_check", &app.address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -15,10 +16,15 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
+pub struct TestApp {
+    pub address: String,
+}
+
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
-    let server = rss_server::startup::run(listener).expect("Failed to bind address");
+    let address = format!("http://127.0.0.1:{}", port);
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    TestApp { address }
 }
